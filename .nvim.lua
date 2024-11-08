5e0f6bc5b97db7d728834276c19c523a59c99c85
A. Balmaseda <aitor.5502.b@gmail.com>
2024-11-08 22:54:30 +0100

    .nvim.lua updated

--- .nvim.lua
+++ .nvim.lua
@@ -4,21 +4,21 @@ wk.add({
 	{
 		"<localleader>fc",
 		function()
-			require("lazyvim.util").terminal("fvtt-config")
+			require("snacks").terminal("fvtt-config")
 		end,
 		desc = "Launch fvtt-config",
 	},
 	{
 		"<localleader>ff",
 		function()
-			require("lazyvim.util").terminal({ "fvtt", "launch", "--world", "gaia" })
+			require("snacks").terminal({ "fvtt", "launch", "--world", "gaia" })
 		end,
 		desc = "Launch foundry with world Gaïa",
 	},
 	{
 		"<localleader>fl",
 		function()
-			require("lazyvim.util").terminal({ "fvtt", "launch" })
+			require("snacks").terminal({ "fvtt", "launch" })
 		end,
 		desc = "Launch foundry",
 	},
@@ -26,7 +26,7 @@ wk.add({
 	{
 		"<localleader>sd",
 		function()
-			require("lazyvim.util").terminal({ "npm", "run", "build:dev" }, { interactive = false })
+			require("snacks").terminal({ "npm", "run", "build:dev" }, { interactive = false })
 			vim.cmd.startinsert()
 		end,
 		desc = "npm run build:dev",
